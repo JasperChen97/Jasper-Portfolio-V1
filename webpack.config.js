480feb5975e3889b404d7b59a3fea36b3ce0526f
Jasper <jasperchen19@gmail.com>
2021-01-18 21:25:38 -0800

    Intial Commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, "client/index.js"),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "client/dist")
+  }
+};
+
